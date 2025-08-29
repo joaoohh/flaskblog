6f653fde9793a923c3e5a857732f858d07f57e11
JHa23-a <jhcordeirohenrique@gmail.com>
2025-08-29 01:39:07 -0300

    feat: add registration and login forms with corresponding templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '8f3b6f4a9c5e1d3a2b7c9e8f6a4b2c1d'
 posts= [
     {
         'author': 'João Henrique Cordeiro',
@@ -26,8 +26,14 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title = 'About')
-
-
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form=form)
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form=form)
 if __name__ == '__main__':
     app.run(debug=True)
 
