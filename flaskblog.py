810cc91a49ac717aa6eecac005fe30671cb6306b
JHa23-a <jhcordeirohenrique@gmail.com>
2025-08-28 22:59:37 -0300

    class 2 update

--- flaskblog.py
+++ flaskblog.py
@@ -2,13 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts= [
+    {
+        'author': 'João Henrique Cordeiro',
+        'title': 'Blog Post 1',
+        'content': 'First Blog Post',
+        'data_posted': 'April 1, 2025'
+    },
+
+    {
+        'author': 'Geovana Remes Gomes Araujo',
+        'title': 'Blog Post 2',
+        'content': 'Second Blog Post',
+        'data_posted': 'April 2, 2025'
+    }
+]
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 @app.route("/about")
 def about():
-    return "<p>about</p>"
+    return render_template('about.html', title = 'About')
 
 
 if __name__ == '__main__':
