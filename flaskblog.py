7cfb064d354dd1997501214cdd4713d09b115e51
JHa23-a <jhcordeirohenrique@gmail.com>
2025-08-29 00:31:54 -0300

    refactor: update templates and styles for improved layout and design

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -30,3 +30,4 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
